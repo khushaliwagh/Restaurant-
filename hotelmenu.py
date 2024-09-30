3a8f4350156c1223f69a49ea54d667a632c92923
khushaliwagh <khushaliwagh76@gmail.com>
2024-09-30 21:19:13 +0530

    Add new files

---
+++ hotelmenu.py
@@ -0,0 +1,17 @@
+# #define the menu of restaurant
+
+# menu={
+#     'pizza':40,
+#     'pasta':50,
+#     'burger':60,
+#     'salat':70,
+#     'coffee':80,
+# }
+# # greet
+
+# print("welcome to our hous")
+# print("pizza: Rs40\npasta: Rs50\nBurger: Rs60\nsalad: Rs70\nCoffee: rs80")
+
+
+
+
